d89a2ac714b71e942f56da53e414de49540d0f5f
Gary Dereuse <garydereuse@hotmail.fr>
2019-05-06 10:04:10 +0100

    Removed useless VoicePlayer in wrong folder, requirements cause outdated, and created init file for System Interactions

---
+++ bot/SysInteraction.py
@@ -0,0 +1,14 @@
+import os
+
+class SystemInteraction:
+    
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def download_and_store_song(song):
+        """
+        This method take an url as a parameters, and optionally a name for the song
+        then use youtube-dl to download it and store it in /home/pi/Botama/bot/song/ folder with default or given name
+        """
+        pass
